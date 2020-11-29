50c74030a9a85b6c3adf862447385f3005385068
SlayerMonkey <gori.mods@gmail.com>
2020-11-29 23:46:45 +0100

    Adding the Order Button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,6 +24,7 @@ const BuildControls = (props) => {
                     disabled={props.disabled[ctrl.type]}
                 />
             ))}
+            <button className={styles.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
         </div>
     )
 }
