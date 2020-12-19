5727db5297a8d38fecaa95429df78a9e5054fc4f
SlayerMonkey <gori.mods@gmail.com>
2020-12-19 12:07:26 +0100

    Finishing the Reducer for Ingredients

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-    <Router>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </Router>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root')
