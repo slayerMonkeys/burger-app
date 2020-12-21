af5313e3afc9e1f4d1fb32a4b316fcf35dd0f051
SlayerMonkey <gori.mods@gmail.com>
2020-12-21 22:38:20 +0100

    Redirect to Improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
@@ -15,18 +15,23 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    onCheckoutCancelled={this.checkoutCancelledHandler}
-                    onCheckoutContinued={this.checkoutContinuedHandler}
+        let summary = <Redirect to='/' />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        onCheckoutCancelled={this.checkoutCancelledHandler}
+                        onCheckoutContinued={this.checkoutContinuedHandler}
                     />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    components={ContactData} />
-            </div>
-        );
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        components={ContactData} />
+                </div>
+
+            )
+        }
+        return summary
     }
 }
 
