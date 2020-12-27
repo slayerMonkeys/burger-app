5c9d56a23c2636d0c0ae5f246188a703a4667018
SlayerMonkey <gori.mods@gmail.com>
2020-12-27 12:25:15 +0100

    Getting a Token from the Backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -93,7 +93,7 @@ class Auth extends Component {
         }
         this.setState({controls: updatedControlsForm, formIsValid: formIsValid});
     }
-    
+
     submitHandler = (event) => {
         event.preventDefault();
         this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
