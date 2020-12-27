47c85559493a94ce00ea6b8956a5909925340348
SlayerMonkey <gori.mods@gmail.com>
2020-12-27 11:39:32 +0100

    Adding an Auth Form

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,130 @@
+import React, {Component} from 'react';
+
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import styles from './Auth.module.css';
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false,
+                errorMessage: null
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 8
+                },
+                valid: false,
+                touched: false,
+                errorMessage: null
+            }
+        },
+        formIsValid: false
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        let errorMessage = null
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+            if (!isValid) errorMessage = 'This input is required'
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+            if (errorMessage === null && !isValid) errorMessage = 'minimum '+rules.minLength+' characters are required'
+        }
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+            if (errorMessage === null && !isValid) errorMessage = 'you need a maximum of '+rules.maxLength+' characters'
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return {
+            isValid: isValid,
+            errorMessage: errorMessage
+        }
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControlsForm = {...this.state.controls}
+        const updatedFormElement = {...updatedControlsForm[controlName]}
+        updatedFormElement.value = event.target.value;
+        if (updatedFormElement.validation) {
+            const { isValid, errorMessage } = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+            updatedFormElement.valid = isValid
+            updatedFormElement.errorMessage = errorMessage
+        }
+        updatedFormElement.touched = true
+        updatedControlsForm[controlName] = updatedFormElement;
+        let formIsValid = true
+        for (let inputIdentifier in updatedControlsForm) {
+            formIsValid = updatedControlsForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({controls: updatedControlsForm, formIsValid: formIsValid});
+    }
+
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            })
+        }
+
+        const form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                errorMessage={formElement.config.errorMessage}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            />
+        ))
+
+        return (
+            <div className={styles.Auth}>
+                <form>
+                    {form}
+                    <Button btnType="Success" disabled={!this.state.formIsValid} >SUBMIT</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -130,6 +130,18 @@ class ContactData extends Component {
             isValid = value.length <= rules.maxLength && isValid;
             if (errorMessage === null && !isValid) errorMessage = 'you need a maximum of '+rules.maxLength+' characters'
         }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+            if (errorMessage === null && !isValid) errorMessage = 'Email is not valid'
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+            if (errorMessage === null && !isValid) errorMessage = 'is not a number'
+        }
         return {
             isValid: isValid,
             errorMessage: errorMessage
