73e0632f3c609bba0588a346081118751a8cd591
SlayerMonkey <gori.mods@gmail.com>
2020-12-27 18:03:22 +0100

    Logging Users Out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -16,12 +16,14 @@ const authSuccess = (state, action) => updateObject(state, {
     loading: false
 })
 const authFail = (state, action) => updateObject(state, { error: action.error, loading: false })
+const authLogout = (state, action) => updateObject(state, { token: null, userId: null })
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         default: return state
     }
 }
