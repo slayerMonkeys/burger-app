4ef1d744a0f766555d5cbfc31c91c8852ece6aca
SlayerMonkey <gori.mods@gmail.com>
2021-01-14 17:21:41 +0100

    Displaying User Specific Orders

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,186 +1,187 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
-import styles from './Auth.module.css';
-import * as actions from '../../store/actions/index';
 import Spinner from "../../components/UI/Spinner/Spinner";
+import styles from "./Auth.module.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component {
-    state = {
-        controls: {
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'Mail Address'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    isEmail: true
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            password: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    placeholder: 'Password'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    minLength: 8
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            }
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Mail Address",
         },
-        formIsValid: false,
-        isSignup: true
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+    isSignup: true,
+  };
+
+  componentDidMount() {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== "/") {
+      this.props.onSetAuthRedirectPath();
+    }
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
     }
 
-    componentDidMount() {
-        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/')
-            return this.props.onSetAuthRedirectPath()
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        let errorMessage = null
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-            if (!isValid) errorMessage = 'This input is required'
-        }
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-            if (errorMessage === null && !isValid) errorMessage = 'minimum '+rules.minLength+' characters are required'
-        }
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-            if (errorMessage === null && !isValid) errorMessage = 'you need a maximum of '+rules.maxLength+' characters'
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return {
-            isValid: isValid,
-            errorMessage: errorMessage
-        }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
     }
 
-    inputChangedHandler = (event, controlName) => {
-        const updatedControlsForm = {...this.state.controls}
-        const updatedFormElement = {...updatedControlsForm[controlName]}
-        updatedFormElement.value = event.target.value;
-        if (updatedFormElement.validation) {
-            const { isValid, errorMessage } = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-            updatedFormElement.valid = isValid
-            updatedFormElement.errorMessage = errorMessage
-        }
-        updatedFormElement.touched = true
-        updatedControlsForm[controlName] = updatedFormElement;
-        let formIsValid = true
-        for (let inputIdentifier in updatedControlsForm) {
-            formIsValid = updatedControlsForm[inputIdentifier].valid && formIsValid;
-        }
-        this.setState({controls: updatedControlsForm, formIsValid: formIsValid});
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
     }
 
-    switchAuthModeHandler = () => {
-        this.setState(prevState => {
-            return { isSignup: !prevState.isSignup }
-        })
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
     }
 
-    render() {
-        const formElementsArray = [];
-        for (let key in this.state.controls) {
-            formElementsArray.push({
-                id: key,
-                config: this.state.controls[key]
-            })
-        }
+    return isValid;
+  }
 
-        let form = formElementsArray.map(formElement => (
-            <Input
-                key={formElement.id}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                errorMessage={formElement.config.errorMessage}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)}
-            />
-        ))
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    this.setState({ controls: updatedControls });
+  };
 
-        if (this.props.loading) return <Spinner />
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignup
+    );
+  };
 
-        let errorMessage = null;
+  switchAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return { isSignup: !prevState.isSignup };
+    });
+  };
 
-        if (this.props.error) {
-            errorMessage = (
-                <p>{this.props.error.message}</p>
-            );
-        }
-
-        let authRedirect = null
-        if (this.props.isAuthenticated) {
-            authRedirect = <Redirect to={this.props.authRedirectPath} />
-        }
-        return (
-            <div className={styles.Auth}>
-                {authRedirect}
-                {errorMessage}
-                <form onSubmit={this.submitHandler}>
-                    {form}
-                    {!this.props.loading? <Button btnType="Success" disabled={!this.state.formIsValid} >SUBMIT</Button>: null}
-                </form>
-                <Button
-                    clicked={this.switchAuthModeHandler}
-                    btnType="Danger">SWITCH TO {this.state.isSignup? 'SIGNIN':'SIGNUP'}</Button>
-            </div>
-        );
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key],
+      });
     }
+
+    let form = formElementsArray.map((formElement) => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = <p>{this.props.error.message}</p>;
+    }
+
+    let authRedirect = null;
+    setTimeout(() => console.log(this.props.isAuthenticated), 2000);
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to={this.props.authRedirectPath} />;
+    }
+
+    return (
+      <div className={styles.Auth}>
+        {authRedirect}
+        {errorMessage}
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+          SWITCH TO {this.state.isSignup ? "SIGNIN" : "SIGNUP"}
+        </Button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        loading: state.auth.loading,
-        error: state.auth.error,
-        isAuthenticated: state.auth.token !== null,
-        buildingBurger: state.burgerBuilder.building,
-        authRedirectPath: state.auth.authRedirectPath
-    }
-}
+const mapStateToProps = (state) => {
+  console.log(state.auth);
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    isAuthenticated: state.auth.token,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
-        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password, isSignup) =>
+      dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath("/")),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,222 +1,244 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import axios from "../../../axios-orders";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 import Button from "../../../components/UI/Button/Button";
-import styles from './ContactData.module.css'
+import styles from "./ContactData.module.css";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import withErrorHandler from "../../../HOC/withErrorHandler/withErrorHandler";
-import * as actionsOrder from '../../../store/actions/index';
+import * as actionsOrder from "../../../store/actions/index";
 
 class ContactData extends Component {
-    state = {
-        orderForm: {
-            name: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Your Name'
-                },
-                value: '',
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            street: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Street'
-                },
-                value: '',
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            zipCode: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Postal Code'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    minLength: 5,
-                    maxLength: 5
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            country: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Country'
-                },
-                value: '',
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'Your Mail'
-                },
-                value: '',
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                errorMessage: null
-            },
-            deliveryMethod: {
-                elementType: 'select',
-                elementConfig: {
-                    options: [
-                        {value: 'fastest', displayValue: 'Fastest'},
-                        {value: 'cheapest', displayValue: 'Cheapest'}
-                    ]
-                },
-                value: 'fastest',
-                valid: true
-            }
+  state = {
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name",
         },
-        formIsValid: false,
-        loading: false
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+        errorMessage: null,
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+        errorMessage: null,
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Postal Code",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
+        errorMessage: null,
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+        errorMessage: null,
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Mail",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+        errorMessage: null,
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" },
+          ],
+        },
+        value: "fastest",
+        valid: true,
+      },
+    },
+    formIsValid: false,
+    loading: false,
+  };
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
+    }
+    const order = {
+      ingredients: this.props.ings,
+      price: this.props.price.toFixed(2),
+      orderData: formData,
+      userId: this.props.userId,
+    };
+    this.props.onOrderBurger(order, this.props.token);
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    let errorMessage = null;
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+      if (!isValid) errorMessage = "This input is required";
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+      if (errorMessage === null && !isValid)
+        errorMessage =
+          "minimum " + rules.minLength + " characters are required";
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+      if (errorMessage === null && !isValid)
+        errorMessage =
+          "you need a maximum of " + rules.maxLength + " characters";
     }
 
-    orderHandler = (event) => {
-        event.preventDefault();
-        const formData = {}
-        for (let formElementIdentifier in this.state.orderForm) {
-            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
-        }
-        const order = {
-            ingredients: this.props.ings,
-            price: this.props.price.toFixed(2),
-            orderData: formData
-        }
-        this.props.onOrderBurger(order, this.props.token);
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+      if (errorMessage === null && !isValid)
+        errorMessage = "Email is not valid";
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        let errorMessage = null
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-            if (!isValid) errorMessage = 'This input is required'
-        }
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-            if (errorMessage === null && !isValid) errorMessage = 'minimum '+rules.minLength+' characters are required'
-        }
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-            if (errorMessage === null && !isValid) errorMessage = 'you need a maximum of '+rules.maxLength+' characters'
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-            if (errorMessage === null && !isValid) errorMessage = 'Email is not valid'
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-            if (errorMessage === null && !isValid) errorMessage = 'is not a number'
-        }
-        return {
-            isValid: isValid,
-            errorMessage: errorMessage
-        }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+      if (errorMessage === null && !isValid) errorMessage = "is not a number";
     }
-
-    inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {...this.state.orderForm}
-        const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
-        updatedFormElement.value = event.target.value;
-        if (updatedFormElement.validation) {
-            const { isValid, errorMessage } = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-            updatedFormElement.valid = isValid
-            updatedFormElement.errorMessage = errorMessage
-        }
-        updatedFormElement.touched = true
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
-        let formIsValid = true
-        for (let inputIdentifier in updatedOrderForm) {
-            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
-        }
-        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
-    }
-
-    render() {
-        const formElementsArray = [];
-        for (let key in this.state.orderForm) {
-            formElementsArray.push({
-                id: key,
-                config: this.state.orderForm[key]
-            });
-        }
-        let form = (
-            <form>
-                {formElementsArray.map(formElement => (
-                        <Input
-                            key={formElement.id}
-                            elementType={formElement.config.elementType}
-                            elementConfig={formElement.config.elementConfig}
-                            value={formElement.config.value}
-                            invalid={!formElement.config.valid}
-                            shouldValidate={formElement.config.validation}
-                            touched={formElement.config.touched}
-                            errorMessage={formElement.config.errorMessage}
-                            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-                    )
-                )}
-                <Button btnType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
-            </form>
-        );
-        if (this.props.loading) {
-            form = <Spinner/>;
-        }
-        return (
-            <div className={styles.ContactData}>
-                <h4>Enter your Contact Data</h4>
-                {form}
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients,
-        price: state.burgerBuilder.totalPrice,
-        loading: state.order.loading,
-        token: state.auth.token
+      isValid: isValid,
+      errorMessage: errorMessage,
+    };
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = { ...this.state.orderForm };
+    const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+    updatedFormElement.value = event.target.value;
+    if (updatedFormElement.validation) {
+      const { isValid, errorMessage } = this.checkValidity(
+        updatedFormElement.value,
+        updatedFormElement.validation
+      );
+      updatedFormElement.valid = isValid;
+      updatedFormElement.errorMessage = errorMessage;
     }
+    updatedFormElement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+    let form = (
+      <form>
+        {formElementsArray.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
+            errorMessage={formElement.config.errorMessage}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
+        <Button
+          btnType="Success"
+          disabled={!this.state.formIsValid}
+          clicked={this.orderHandler}
+        >
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={styles.ContactData}>
+        <h4>Enter your Contact Data</h4>
+        {form}
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onOrderBurger: (orderData, token) => dispatch(actionsOrder.purchaseBurger(orderData, token))
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading,
+    token: state.auth.token,
+    userId: state.auth.userId,
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onOrderBurger: (orderData, token) =>
+      dispatch(actionsOrder.purchaseBurger(orderData, token)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(ContactData, axios));
