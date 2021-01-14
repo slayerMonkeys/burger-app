8a3dfc41605cc9184b73a78b85ad1aae7622cdf9
SlayerMonkey <gori.mods@gmail.com>
2021-01-14 21:10:58 +0100

    NavigationItems Unit Test Continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -12,8 +12,17 @@ configure({
 jest.setTimeout(10000);
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItem /> elements if authenticated", () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
